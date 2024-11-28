69fb5ddd15c02dc884324d616e7659542fce669f
skid <skid@kinkpad.local>
2024-11-28 22:59:19 +0100

    togli roba da App

--- java/src/main/java/com/scuola/App.java
+++ java/src/main/java/com/scuola/App.java
@@ -1,27 +1,9 @@
 package com.scuola;
 
-import com.scuola.esercizi.Stringhe;
-
 public final class App {
     private App() {
     }
 
     public static void main(String[] args) {
-        String s1 = Stringhe.accorcia("Ciao tt bn?", 20); // =>  "Ciao tt bn?"
-        String s2 = Stringhe.accorcia("Ciao tt bn?", 6); // => "Cia..."
-        System.out.println(s1);
-        System.out.println(s2);
-
-        Stringhe.incornicia("Ciao tt bn?");
-        /*  ###############
-         *  # Ciao tt bn? #
-         *  ###############
-         */ 
-
-         Stringhe.incornicia("hey");
-        /*  #######
-         *  # hey #
-         *  #######
-         */ 
     }
 }
