4367da115106c3354bd692f3a1caef1c4ae9e38c
skid <skid@kinkpad.local>
2025-02-09 12:41:50 +0100

    implementata tutta Read (da testare!)

--- java/src/main/java/com/scuola/util/Read.java
+++ java/src/main/java/com/scuola/util/Read.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-
 /*
  * Read e' una classe di utilita' che semplifica la lettura di valori da linea di comando.
  * La sua filosofia e' quella di continuare a chiedere input all'utente fino a quando
@@ -20,11 +19,15 @@ public final class Read {
 
     private static final String STDIN_ERROR = "ERROR: could not read from stdin";
     private static final String INTPARSE_ERROR = "! Input can't be parsed to integer";
-    private static final String LESSTHAN_ERROR = "! Should be less than %s";
-    private static final String MORETHAN_ERROR = "! Should be more than %s";
+    private static final String FLOATPARSE_ERROR = "! Input can't be parsed to a number";
+    private static final String LESSTHAN_ERROR = "! Should be <= %s";
+    private static final String MORETHAN_ERROR = "! Should be >= %s";
     private static final String BETWEEN_ERROR = "! Should be between %s and %s";
     private static final String YESORNO_ERROR = "! Write '%s' or '%s'";
     private static final String SELECT_OPTION = "[%s] %s";
+    private static final String STRMAX_ERROR = "! Lenght sould be <= %s characters";
+    private static final String STRMIN_ERROR = "! Lenght be more >= %s characters";
+    private static final String STRMINMAX_ERROR = "! Lenght be between %s and $s characters";
 
     /*
      * 0) stampa in output il messaggio
@@ -79,13 +82,7 @@ public final class Read {
             try {
                 // provo a convertire l'input in un intero
                 input = Integer.parseInt(s);
-                error = !(input >= min);
-                // equivalente a questo:
-                // if (input >= min) {
-                //     error = false;
-                // } else {
-                //     error = true;
-                // }
+                error = input < min;
                 if (error) System.out.println(MORETHAN_ERROR.formatted(min));
             } catch (NumberFormatException e) {
                 System.out.println(INTPARSE_ERROR);
@@ -104,7 +101,7 @@ public final class Read {
             String s = Read.string(message);
             try {
                 input = Integer.parseInt(s);
-                error = !(input <= max);
+                error = input > max;
                 if (error) System.out.println(LESSTHAN_ERROR.formatted(max));
             } catch (NumberFormatException e) {
                 System.out.println(INTPARSE_ERROR);
@@ -123,7 +120,7 @@ public final class Read {
             String s = Read.string(message);
             try {
                 input = Integer.parseInt(s);
-                error = !(input >= min && input <= max);
+                error = input < min || input > max;
                 if (error) System.out.println(BETWEEN_ERROR.formatted(min, max));
             } catch (NumberFormatException e) {
                 System.out.println(INTPARSE_ERROR);
@@ -143,37 +140,15 @@ public final class Read {
      */
 
     public static boolean yesOrNo(String message, String yes, String no) {
-        boolean input = false;
-        boolean error;
+        String s = null;
         do {
-            String s = Read.string(message + "(%s/%s)".formatted(yes, no));
-            if (s.equals(yes)) {
-                input = true;
-                error = false;
-            } else if (s.equals(no)) {
-                input = false;
-                error = false;
-            } else {
-                error = true;
-                System.out.println(YESORNO_ERROR.formatted(yes, no));
-            }
-        } while (error);
+            if (s != null) System.out.println(String.format(YESORNO_ERROR, yes, no));
+            s = Read.string(message);
+        } while (!s.equals(yes) && !s.equals(no));
 
-        return input;
+        return s.equals(yes);
     }
 
-    // alternativa piu' breve
-    // public static boolean yesOrNo(String message, String yes, String no) {
-    //     String s = null;
-    //     do {
-    //         if (s != null) {
-    //             System.out.println(String.format(YESORNO_ERROR, yes, no));
-    //         }
-    //         s = Read.string(message);
-    //     } while (!(s.equals(yes) || s.equals(no)));
-
-    //     return s.equals(yes);
-    // }
 
     /*
      * Permette all'utente di selezionare una delle opzioni (passate come ArrayList)
@@ -190,33 +165,58 @@ public final class Read {
         return input;
     }
 
-    /*
-     * QUA FINISCONO GLI ESERCIZI INTERESSANTI:
-     * il resto dei metodi sono utili ma la loro implementazione
-     * e' molto simile ad altri gia' implementati e quindi non richiesta
-     */
     /* Leggi in input una stringa di lunghezza >= minLength */
     public static String stringMin(String message, int minLength) {
-        // TODO
-        return "";
+        String s = "";
+        boolean error;
+        do {
+            s = Read.string(message);
+            error = s.length() < minLength;
+            if (error) System.out.println(STRMIN_ERROR.formatted(minLength));
+        } while (error);
+        return s;
     }
 
     /* Leggi in input una stringa di lunghezza <= maxLength */
     public static String stringMax(String message, int maxLength) {
-        // TODO
-        return "";
+        String s = "";
+        boolean error;
+        do {
+            s = Read.string(message);
+            error = s.length() > maxLength;
+            if (error) System.out.println(STRMAX_ERROR.formatted(maxLength));
+        } while (error);
+        return s;
     }
 
-    /* Leggi in input una stringa di lunghezza compresa tra minLenght e  maxLength (estremi inclusi) */
+    /* Leggi in input una stringa di lunghezza compresa tra minLength e  maxLength (estremi inclusi) */
     public static String stringMinMax(String message, int minLength, int maxLength) {
-        // TODO
-        return "";
+        String s = "";
+        boolean error;
+        do {
+            s = Read.string(message);
+            error = s.length() > maxLength || s.length() < minLength;
+            if (error) System.out.println(STRMINMAX_ERROR.formatted(minLength, maxLength));
+        } while (error);
+        return s;
     }
 
     /* Leggi in input un numero con la virgola */
     public static float number(String message) {
-        // TODO
-        return 0;
+        float input = 0;
+        boolean error;
+
+        do {
+            String s = Read.string(message);
+            try {
+                input = Float.parseFloat(s);
+                error = false;
+            } catch (NumberFormatException e) {
+                System.out.println(FLOATPARSE_ERROR);
+                error = true;
+            }
+        } while (error);
+        return input;
     }
 
     /* Leggi in input un numero con la virgola che deve essere >= min */
@@ -224,16 +224,13 @@ public final class Read {
         float input = 0;
         boolean error;
         do {
-
             String s = Read.string(message);
             try {
-
                 input = Float.parseFloat(s);
-                error = !(input <= min);
-
-                if (error) System.out.println(LESSTHAN_ERROR.formatted(min));
+                error = input < min;
+                if (error) System.out.println(MORETHAN_ERROR.formatted(min));
             } catch (NumberFormatException e) {
-                System.out.println(INTPARSE_ERROR);
+                System.out.println(FLOATPARSE_ERROR);
                 error = true;
             }
         } while (error);
@@ -243,20 +240,39 @@ public final class Read {
 
     /* Leggi in input un numero con la virgola che deve essere <= max */
     public static float numberMax(String message, float max) {
-        // TODO
-        return 0;
+        float input = 0;
+        boolean error;
+        do {
+            String s = Read.string(message);
+            try {
+                input = Float.parseFloat(s);
+                error = input > max;
+                if (error) System.out.println(LESSTHAN_ERROR.formatted(max));
+            } catch (NumberFormatException e) {
+                System.out.println(FLOATPARSE_ERROR);
+                error = true;
+            }
+        } while (error);
+
+        return input;
     }
 
     /* Leggi in input un numero con la virgola che deve essere compreso tra max e min (estremi inclusi) */
     public static float numberMinMax(String message, float min, float max) {
-        boolean controllo=true;
-        float n=0;
-        while (controllo){
-            controllo = false;
-            String input = Read.string(message);
-            try { n=Float.parseFloat(input); if (n>max || n<min){controllo=true;}}
-            catch (Exception e) { System.out.println("ERRORE DI CONVERSIONE A FLOAT"); controllo=true; }
-        }
-        return n;
+        float input = 0;
+        boolean error;
+        do {
+            String s = Read.string(message);
+            try {
+                input = Float.parseFloat(s);
+                error = input < min || input > max;
+                if (error) System.out.println(BETWEEN_ERROR.formatted(min, max));
+            } catch (NumberFormatException e) {
+                System.out.println(FLOATPARSE_ERROR);
+                error = true;
+            }
+        } while (error);
+
+        return input;
     }
 }
